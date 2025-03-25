b2c9c1a7a70c2572d5669738bd09add27ddeee2f
Tao Xiao <>
2025-03-25 16:49:49 +0800

    u

--- ai/ai/ch_09/chat_bot_with_history.py
+++ ai/ai/ch_09/chat_bot_with_history.py
@@ -3,16 +3,18 @@ from langchain_core.messages import HumanMessage
 from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI
 
+## 按照 session_id 存储会话历史
+session_store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in session_store:
+        session_store[session_id] = InMemoryChatMessageHistory()
+    return session_store[session_id]
+
+
 ## 能记住聊天历史的机器人
 ## 这里使用了基于内存的消息历史记忆
 def run_chat_bot_with_history():
-    session_store = {}
-
-    def get_session_history(session_id: str) -> BaseChatMessageHistory:
-        if session_id not in session_store:
-            session_store[session_id] = InMemoryChatMessageHistory()
-        return session_store[session_id]
-
     model_with_msg_history = RunnableWithMessageHistory(
         ChatOpenAI(model='gpt-4o-mini'),
         get_session_history
