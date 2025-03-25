4b1f0818605d9bd06304a56b94209d752f55f11e
Tao Xiao <>
2025-03-25 15:22:15 +0800

    a

--- ai/ai/ch08/hello_chat_model.py
+++ ai/ai/ch08/hello_chat_model.py
@@ -1,8 +1,9 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
 
 messages = [
     SystemMessage('Translate the following from English to Chinese'),
@@ -83,10 +84,50 @@ def run_with_output_parser():
         print(resp_content, end='|')
 
 
+## 试试【JsonOutputParser】
+def run_with_JsonOutputParser():
+    class Book(BaseModel):
+        title: str = Field(description='作品名称')
+        description: str = Field(description='作品简介')
+
+    parser = JsonOutputParser(pydantic_object=Book)
+
+    # 指定要求返回的格式
+    prompt = PromptTemplate(
+        template='列举 3 部 {author} 的作品。\n{my_format_instructions}',
+        input_variables=['author'],
+        partial_variables={'my_format_instructions': parser.get_format_instructions()}
+    )
+
+    model = ChatOpenAI(model='gpt-4o-mini')
+
+    chain = prompt | model | parser
+
+    # 返回的 result 直接就是满足 Book 的格式的 JSON 字符串，如下
+    # [
+    #     {
+    #         "title": "狂人日记",
+    #         "description": "这是鲁迅的第一部小说，也是中国现代文学的重要作品，通过一个狂人的视角揭示了社会的黑暗和人性的扭曲。"
+    #     },
+    #     {
+    #         "title": "阿Q正传",
+    #         "description": "此作品通过对阿Q的生动描绘，讽刺了社会的虚伪和封建主义，深刻反映了当时中国社会的病态。"
+    #     },
+    #     {
+    #         "title": "故乡",
+    #         "description": "这篇小说表达了鲁迅对故乡的复杂情感，以及对乡土社会的批判，展现了农村的落后与残酷。"
+    #     }
+    # ]
+    result = chain.invoke({'author': '鲁迅'})
+    print(result)
+
+
+
 ## 假定环境变量 OPENAI_API_KEY 已经存在
 if __name__ == '__main__':
     # run_init_chat_model()
     # run_chat_openai()
     # run_chat_openai_async()
     # run_with_prompt_template()
-    run_with_output_parser()
+    # run_with_output_parser()
+    run_with_JsonOutputParser()
